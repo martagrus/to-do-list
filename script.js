401131df559baab9b8d7808b83f07d44eed68b4e
Marta Grus <mrs_marta@o2.pl>
2019-07-21 19:46:59 +0200

    ppr

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-let $list, $input, $addBtn, $closePU, $cancelBtn;
+let $list, $input, $addBtn, $closePU, $cancelBtn, $popInput, $changeBtn;
 let nextId = 1;
+let editedTextId;
 
 let firstList = ['dog', 'cat', 'food'];
 
@@ -7,6 +8,7 @@ main = () => {
     prepareDOMElements();
     prepareDOMEvents();
     prepareInitialList();
+    openPopup();
     closePopup();
 }
 
@@ -14,18 +16,18 @@ prepareDOMElements = () => {
     $list = document.getElementById('list');
     $input = document.getElementById('newInput');
     $addBtn = document.getElementById('addBtn');
-    
+    $popInput = document.getElementById('ppContent');
+    $closePU = document.getElementById('closePU');
+    $cancelBtn = document.getElementById('cancelBtn');
+    $changeBtn = document.getElementById('changeBtn');
 }
 
 prepareDOMEvents = () => {
     $addBtn.addEventListener('click', addButtonClickHandler);
-    $closePU = document.getElementById('closePU');
     $closePU.addEventListener('click', closePopup);
-    $cancelBtn = document.getElementById('cancelBtn');
     $cancelBtn.addEventListener('click', closePopup);
-   // $changeBtn = document.getElementById('changeBtn');
-   // $changeBtn.addEventListener('click', closePopup);
     $list.addEventListener('click', listClickManager);
+    $changeBtn.addEventListener('click', acceptChangeHandler);
 }
 
 prepareInitialList = () => {
@@ -59,23 +61,6 @@ createNewButton = (btnRole, btnId) => {
     const newButton = document.createElement('button');
     newButton.innerText = btnRole;
     newButton.id = btnId;
-    /*if (btnRole === 'Delete') {
-        newButton.addEventListener ('click', () => {
-            document.getElementById(elementId).remove();
-        });
-    }
-    if (btnRole === 'Done') {
-        newButton.addEventListener ('click', () => {
-            document.getElementById(elementId).style.background = "#db5858";
-            document.getElementById(elementId).style.textDecoration = "line-through";
-        });
-    }
-    if (btnRole === 'Edit') {
-        newButton.addEventListener ('click', () => {
-            document.getElementById('popUp').style.display = "block";
-            editListElement;
-        });
-    }*/
 
     return newButton;
 }
@@ -89,46 +74,30 @@ listClickManager = (eventObject) => {
     } else if (action === 'delete') {
         document.getElementById(elementId).remove();
     } else if (action === 'edit') {
-        editListElement();
+        editListElement(elementId);
         }
     } 
 
-    // Rozstrzygnięcie co dokładnie zostało kliknięte i wywołanie odpowiedniej funkcji
-    // event.target.parentElement.id
-    // if(event.target.className === 'edit') { editListElement(id) }
-
-
-editListElement = (elementId) => {
-    let oldText = elementId.innerText
-    console.log(oldText);
-    
-
-
-    document.getElementById('popUp').style.display = "block";
-    /*$editedText = document.getElementById(nextId).innerHTML;
-    $editedText = document.querySelector(inpupt.value);
-    $editedText.id = ("edited-" + nextId);*/
+editListElement = (id) => {
+    let editedText = document.getElementById(id).firstChild;
+    editedTextId = id;
+    openPopup();
+    $popInput.value = editedText.textContent;
+    acceptChangeHandler();
     }
 
-    // Pobranie informacji na temat zadania
-    // Umieść dane w popupie
-    //const edtBtn = document.createElement('button');
-    //edtBtn.innerText = 'Edit';
-
-
-
-function addDataToPopup(/* Title, author, id */) {
-    // umieść informacje w odpowiednim miejscu w popupie
+acceptChangeHandler = (editedText) => {
+    //let editedText = document.getElementById(id);
+    editedText.innerText = $popInput.value;
+    closePopup();
 }
 
-function acceptChangeHandler() {
-    // pobierz dane na temat zadania z popupu (id, nowyTitle, nowyColor ...)
-    // Następnie zmodyfikuj element listy wrzucając w niego nowyTitle, nowyColor...
-    // closePopup()
+openPopup = () => {
+    document.getElementById('popUp').style.display = "block";
 }
 
 closePopup = () => {
-        document.getElementById('popUp').style.display = "none";
-    }
+    document.getElementById('popUp').style.display = "none";
+}
 
 document.addEventListener('DOMContentLoaded', main);
